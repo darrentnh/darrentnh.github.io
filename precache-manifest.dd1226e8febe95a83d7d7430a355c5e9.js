59356311eadba7d10402ba7714da936b6ad17917
Darren Tan <darrentan95@gmail.com>
2020-05-09 21:13:59 +0800

    Updates

---
+++ precache-manifest.dd1226e8febe95a83d7d7430a355c5e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880fee6bacf27ffeef6ad8ddd417d473",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a027dc5326687735cd2f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8afd94b234b415b821c9",
+    "url": "/static/js/2.5f96925a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f96925a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a027dc5326687735cd2f",
+    "url": "/static/js/main.a9780203.chunk.js"
+  },
+  {
+    "revision": "9d8c3560c1c5e0e31b89",
+    "url": "/static/js/runtime-main.00521afc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
